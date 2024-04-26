871cdbd396d504a384264246bb838d8e3a18afed
limbo cingo <mrcingosup@gmail.com>
2024-04-26 12:27:27 +0000

    Main bot

--- client.py
+++ client.py
@@ -1,15 +1,13 @@
 import os
-
 import discord
 
-load_
-
-class MyClient(discord.Client):
+class PirateBot(discord.Client):
+    
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
 
-client = MyClient(intents=discord.Intents.all())
+client = PirateBot(intents=discord.Intents.all())
 client.run(os.getenv('BOT_TOKEN'))
